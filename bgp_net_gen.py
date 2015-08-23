980fd55417db5c0e4fa1ca54f88ceca7147d27ea
slaffer-au <slaffer.au@gmail.com>
2015-08-24 00:25:34 +1000

    Fixed argparse for mask by converting to int type

--- bgp_net_gen.py
+++ bgp_net_gen.py
@@ -4,12 +4,16 @@ import itertools
 import argparse
 
 def reverse_enumerate(iterable):
-    """
-    Enumerate over an iterable in reverse order while retaining proper indexes
-    """
+    '''
+    Enumerate over an iterable in reverse order while retaining proper indexes.
+    '''
+    
     return itertools.izip(reversed(xrange(len(iterable))), reversed(iterable))
 
 def str_to_int(prefix):
+    '''
+    Takes the network string and converts it to a list of integers.
+    '''
     
     octets = prefix.split(".")
     
@@ -17,37 +21,40 @@ def str_to_int(prefix):
     
     for octet in octets:
         octet = int(octet)
-        type (octet)
         p_octets.append(octet)
         
     return p_octets
 
 def next_network(prefix):
+    '''
+    Takes the last host address of the subnet and increment to get first address
+    of the next subnet. 
+    '''
     
     prefix[3] = prefix[3] + 2
     
     prefix = check_network(prefix)
-    
-    # print prefix
-    
+
     return prefix   
 
 def check_network(prefix): 
+    '''
+    Checks to see if the network address is valid. If not, the invalid octet
+    resets to 0 and increments the next (more significant) octet by 1.
+    '''
     
     for count, p in reverse_enumerate(prefix):
      
         if p > 255:
             prefix[count] = 0
             prefix[count-1] += 1
-            print
-    
-    # print count, count -1, p     
-    # print prefix
     
     return prefix
     
 def net_print(network):
-    
+    '''
+    Print the network string and strings if set.
+    '''
     network = str(network)
     
     if args.before or args.after:
@@ -58,47 +65,60 @@ def net_print(network):
     
 
 
-### Argument Parser ###
-
-parser = argparse.ArgumentParser(description="Network Generator.")
-parser.add_argument('--first', '-f', action="store", dest="first_prefix", help="First Prefix")
-parser.add_argument('--mask', '-m', action="store", dest="mask", help="Subnet Mask in CIDR 'slash' notation")                    
-parser.add_argument('--prefixes', '-p', action="store", dest="prefixes", help="Amount of prefixes")                    
-parser.add_argument('--before', '-b', action="store", dest="before", help="Text to prepend before network string")
-parser.add_argument('--after', '-a', action="store", dest="after", help="Text to append after network string")                
-
-args = parser.parse_args()   
+ 
 
 ### Main ###
-
-if not args.first:
-    first_prefix = raw_input("What is the first prefix? ")
-
-if not args.mask:
-    mask = raw_input("What is the subnet mask in CIDR notation? ")
-
-if not args.prefixes:
-    prefixes = input("How many prefixes? ")
-
-
-p_octets = str_to_int(first_prefix)
-
-p_count = 0
-
-
-while p_count < prefixes:
+try:
     
-    p_count += 1
+    ### Argument Parser ###
+    parser = argparse.ArgumentParser(description="Network Generator.")
+    parser.add_argument('--first', '-f', action="store", dest="first_prefix", help="First Prefix")
+    parser.add_argument('--mask', '-m', action="store", dest="mask", help="Subnet Mask in CIDR 'slash' notation")                    
+    parser.add_argument('--prefixes', '-p', action="store", dest="prefixes", help="Amount of prefixes")                    
+    parser.add_argument('--before', '-b', action="store", dest="before", help="Text to prepend before network string")
+    parser.add_argument('--after', '-a', action="store", dest="after", help="Text to append after network string")                
     
-    network = ipcalc.Network("%s.%s.%s.%s/%s" % (p_octets[0], p_octets[1], p_octets[2], p_octets[3], mask))
-    net_print(network)
+    args = parser.parse_args()  
+
+
+    ### Variable Setup ###   
+    if not args.first_prefix:
+        first_prefix = raw_input("What is the first prefix? ")
+    else:
+        first_prefix = args.first_prefix
+        
+    if not args.mask:
+        mask = raw_input("What is the subnet mask in CIDR notation? ")
+    else:
+        mask = args.mask
+        
+    if not args.prefixes:
+        prefixes = input("How many prefixes? ")
+    else:
+        prefixes = int(args.prefixes)
+        
+    p_count = 0
+
     
-    h_last = network.host_last()
-    p_network = str_to_int(str(h_last))
+    ### List Setup ###
+    p_octets = str_to_int(first_prefix)
+
+
+    ### Printing Loop ###   
+    while p_count < prefixes:
+        
+        p_count += 1
+        
+        network = ipcalc.Network("%s.%s.%s.%s/%s" % (p_octets[0], p_octets[1], p_octets[2], p_octets[3], mask))
+        net_print(network)
+        
+        h_last = network.host_last()
+        p_network = str_to_int(str(h_last))
+        
+        p_octets = next_network(p_network)
     
-    p_octets = next_network(p_network)
-    
-    
+except ValueError, e:
+    print e    
 
 
     
