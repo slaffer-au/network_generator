e168a046186695d80c8927fb207e6c82dcad55d1
slaffer-au <slaffer.au@gmail.com>
2015-08-23 23:57:21 +1000

    Adding argparse

--- bgp_net_gen.py
+++ bgp_net_gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import ipcalc
 import itertools
-import subprocess
+import argparse
 
 def reverse_enumerate(iterable):
     """
@@ -46,18 +46,40 @@ def check_network(prefix):
     
     return prefix
     
+def net_print(network):
+    
+    network = str(network)
+    
+    if args.before or args.after:
+        print args.before+network+args.after
+    
+    else:
+        print network
+    
 
+
+### Argument Parser ###
+
+parser = argparse.ArgumentParser(description="Network Generator.")
+parser.add_argument('--first', '-f', action="store", dest="first_prefix", help="First Prefix")
+parser.add_argument('--mask', '-m', action="store", dest="mask", help="Subnet Mask in CIDR 'slash' notation")                    
+parser.add_argument('--prefixes', '-p', action="store", dest="prefixes", help="Amount of prefixes")                    
+parser.add_argument('--before', '-b', action="store", dest="before", help="Text to prepend before network string")
+parser.add_argument('--after', '-a', action="store", dest="after", help="Text to append after network string")                
+
+args = parser.parse_args()   
 
 ### Main ###
 
-first_prefix = "10.10.10.0"
-#first_prefix = raw_input("What is the first network? ")
+if not args.first:
+    first_prefix = raw_input("What is the first prefix? ")
 
-mask = "30"
-#mask = raw_input("What is the subnet mask in CIDR notation? ")
+if not args.mask:
+    mask = raw_input("What is the subnet mask in CIDR notation? ")
 
-prefixes = 300
-#prefixes = input("How many prefixes? ")
+if not args.prefixes:
+    prefixes = input("How many prefixes? ")
+
 
 p_octets = str_to_int(first_prefix)
 
@@ -69,14 +91,13 @@ while p_count < prefixes:
     p_count += 1
     
     network = ipcalc.Network("%s.%s.%s.%s/%s" % (p_octets[0], p_octets[1], p_octets[2], p_octets[3], mask))
-    print network
+    net_print(network)
     
     h_last = network.host_last()
     p_network = str_to_int(str(h_last))
     
     p_octets = next_network(p_network)
     
-    # print p_octets
     
 
 
